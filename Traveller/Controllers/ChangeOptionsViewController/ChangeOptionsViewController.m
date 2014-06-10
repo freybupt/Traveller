cef21e167d41b09654da2991b7294445e7ba3121
admin <admin@admins-Mac-mini.local>
2014-06-10 15:39:25 -0700

    - Added support for getting the Data from the server and sorting it according to some criteria - Added 2 more test files to avoid saturating the servers with GET requests

--- Traveller/Controllers/ChangeOptionsViewController/ChangeOptionsViewController.m
+++ Traveller/Controllers/ChangeOptionsViewController/ChangeOptionsViewController.m
@@ -11,7 +11,7 @@
 @interface ChangeOptionsViewController ()
 @property (nonatomic) BOOL isAConnectionOpen;
 
--(void)sendGetRequest:(Trip*)trip;
+-(NSData*)sendGetRequest:(Trip*)trip;
 @end
 
 @implementation ChangeOptionsViewController
@@ -31,8 +31,9 @@
     if(!self.isAConnectionOpen){
         NSLog(@"connection started");
         if (self.trip){
-            self.title = [self.trip.toEvent.eventType integerValue] == EventTypeHotel? @"HOTEL OPTIONS":@"FLIGHT OPTIONS";
-            [self sendGetRequest:self.trip];
+            //self.title = [self.trip.toEvent.eventType integerValue] == EventTypeHotel? @"HOTEL OPTIONS":@"FLIGHT OPTIONS";
+            //NSData* serverResponse = [self sendGetRequest:self.trip];
+            [self processEventChange:nil];
         } else {
             UIAlertView *errorMessage = [[UIAlertView alloc] initWithTitle: @"Error" message: @"Invalid trip selected. Please go back to the previous screen" delegate: nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
             [errorMessage show];
@@ -43,19 +44,37 @@
     //[[DataManager sharedInstance] registerBridgedMoc:self.managedObjectContext];
     [self hideActivityIndicator];
 }
+/*-(IBAction)calculateTrip:(id)sender {
+ [self calculateTripFromClient:sender];
+ [self calculateTripFromServer:sender usingResponse:nil];
+ }*/
+
+-(void)processEventChange:(NSData*)serverData{
+    
+    //This part is to avoid saturating the server with GET requests
+    //TODO: comment or uncomment this section as needed
+    BOOL isHotel = [self.trip.toEvent.eventType integerValue] == EventTypeHotel? YES:NO;
+    if(isHotel){
+        NSString *filePath = [[NSBundle mainBundle]pathForResource:@"ShowHotels9" ofType:@"json"];
+        NSString *jsonDataInStr = [[NSString alloc] initWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+        serverData = [jsonDataInStr dataUsingEncoding:NSUTF8StringEncoding];
+    } else {
+        NSString *filePath = [[NSBundle mainBundle]pathForResource:@"ShowFlights166" ofType:@"json"];
+        NSString *jsonDataInStr = [[NSString alloc] initWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+        serverData = [jsonDataInStr dataUsingEncoding:NSUTF8StringEncoding];
+    }
+
+    if (!serverData){
+        serverData = [self sendGetRequest:self.trip];
+    } else {
+        NSArray *optionsFromServer = [self returnArrayOfEventsWith:serverData usingContext:[self managedObjectContext] sortedBy:@"duration"];
+        NSLog(@"many options here....%@", optionsFromServer);
+    }
+}
 
 
 
-//function: perform operations needed{
-    //use sendGet to get a list of events from an eventID sent to the user
-    //sort the events based on the current's tab description
-    //display to the user the events based on the sorting algorithm
-//}
-
-
-
-
-- (void) sendGetRequest:(Trip*)trip{
+- (NSData*) sendGetRequest:(Trip*)trip{
     UIAlertView *loadingMessage = [[UIAlertView alloc] initWithTitle: @"Loading" message: @"Please wait while your options load. This may take a few moments" delegate: nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
     [loadingMessage show];
     
@@ -79,7 +98,7 @@
     
     //create two block variables to store the response
     //__block NSError* errorMain;
-    __block NSData *responseAsync;//right now it is being done synchronously
+    __block NSData *responseAsync;
     
     //use asynch connection to send a POST request
     [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error){
@@ -95,16 +114,17 @@
         {
             [loadingMessage dismissWithClickedButtonIndex:0 animated:YES];
             responseAsync = data;
-            NSString *theReply = [[NSString alloc]initWithBytes:[responseAsync bytes] length:[responseAsync length] encoding:NSUTF8StringEncoding];
-            NSLog(@"\n\n\n\n %@", theReply);
+            //NSString *theReply = [[NSString alloc]initWithBytes:[responseAsync bytes] length:[responseAsync length] encoding:NSUTF8StringEncoding];
+           // NSLog(@"\n\n\n\n %@", theReply); [self calculateTripFromServer:nil usingResponse:responseAsync];
             UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"Success!" message: @"Here are your possible options" delegate: nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
             [alert show];
             self.isAConnectionOpen = NO;
+            [self processEventChange:responseAsync];
             
         }
         
     }];
-    
+    return responseAsync;
 }
 
 - (void)didReceiveMemoryWarning
@@ -131,5 +151,39 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+#pragma mark - Auxiliary functions
+-(NSArray*) returnArrayOfEventsWith:(NSData*)jsonData usingContext:(NSManagedObjectContext*)context sortedBy:(NSString*)sortingKey{
+    NSError *error;
+    
+    NSString *theReply = [[NSString alloc]initWithBytes:[jsonData bytes] length:[jsonData length] encoding:NSUTF8StringEncoding];
+    NSLog(@"\n\n\n\n %@", theReply);
+    NSDictionary *jsonDic = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&error];
+    if([jsonDic objectForKey:@"Error Message"]){
+        NSString* errorMessage = [jsonDic objectForKey:@"Error Message"];
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"Error!" message: errorMessage delegate: nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+        return nil;
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"Success!" message: @"Your best travelling options are being displayed now." delegate: nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+
+    }
+    NSString* jsonDicKey = [self.trip.toEvent.eventType integerValue] == EventTypeHotel? @"hotelList":@"flightList";
+    NSArray *optionsFromServer = [[NSArray arrayWithObject:[jsonDic objectForKey:jsonDicKey]]objectAtIndex:0];
+    //NSLog(@"%@", planSteps);
+    NSSortDescriptor *descriptor = [[NSSortDescriptor alloc] initWithKey:sortingKey  ascending:YES];
+    optionsFromServer=[optionsFromServer sortedArrayUsingDescriptors:[NSArray arrayWithObjects:descriptor,nil]];
+    NSLog(@"this is it: %@",[optionsFromServer description]);
+    
+    //for (NSDictionary* option in optionsFromServer){
+    //}
+    
+    //to test: the output must be equal to 3
+    return optionsFromServer;
+    
+}
+
+
 
 @end
--- Traveller/Controllers/PlanTripViewController/PlanTripViewController.m
+++ Traveller/Controllers/PlanTripViewController/PlanTripViewController.m
@@ -559,9 +559,9 @@ static NSInteger kHotelCellFullHeight = 540;
     NSData *jsonDataOut = [self printToJsonAtCity:departureCity withEvents:events atContext:self.managedObjectContext];
     //This is a file in order to avoid requesting information from the server during testing periods to save time
     //TODO: comment or uncomment this sectino as needed
-    //NSString *filePath = [[NSBundle mainBundle]pathForResource:@"ServerResponse" ofType:@"json"];
-    //NSString *jsonDataInStr = [[NSString alloc] initWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
-    //jsonResponse = [jsonDataInStr dataUsingEncoding:NSUTF8StringEncoding];
+    NSString *filePath = [[NSBundle mainBundle]pathForResource:@"ServerResponse" ofType:@"json"];
+    NSString *jsonDataInStr = [[NSString alloc] initWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+    jsonResponse = [jsonDataInStr dataUsingEncoding:NSUTF8StringEncoding];
     
     
     if(!jsonResponse){ //if there is nothing from the server, ask for it
